d78f28500e1f016970a1317085c0a6eb9ac45c9d
Karina Kryklyva <karin.krikliva@gmail.com>
2025-06-04 09:24:54 +0300

    enhancer for devtools added

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import {
 	isLoadingReducer,
@@ -14,4 +14,6 @@ const reducer = combineReducers({
 	isLoading: isLoadingReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
